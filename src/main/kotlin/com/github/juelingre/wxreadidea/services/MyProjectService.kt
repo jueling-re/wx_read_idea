ca27d7d2f02660b65e69568d75ad7f07395591da
GitHub Action <action@github.com>
2023-03-24 02:59:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/juelingre/wxreadidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/juelingre/wxreadidea/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.juelingre.wxreadidea.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.juelingre.wxreadidea.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
